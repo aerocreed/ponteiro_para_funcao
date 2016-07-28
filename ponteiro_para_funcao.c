a51dbceb356237fabc80a4dc6c1087f1e6c7073e
Germano Martins <germano_nino@hotmail.com>
2016-07-27 21:31:54 -0300

    Invocando funções dinamicamente

---
+++ ponteiro_para_funcao.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#define TAM 5
+
+void funcao1(){
+    printf("FUNCAO 1\n");
+}
+void funcao2(){
+    printf("FUNCAO 2\n");
+}
+
+void funcao3(){
+    printf("FUNCAO 3\n");
+}
+void funcao4(){
+    printf("FUNCAO 4\n");
+}
+
+void funcao5(){
+    printf("FUNCAO 5\n");
+}
+
+void invoca(int (*func)()){
+    (*func)();
+}
+
+int main()
+{
+    int p[TAM] = {funcao1, funcao2, funcao3, funcao4, funcao5};
+    int i, opcao;
+    char *nomes[30] = {"Funcao 1", "Funcao 2", "Funcao 3", "Funcao 4", "Funcao 5"};
+
+    printf("ESCOLHA UMA OPCAO\n");
+    for(i=0; i<TAM; i++)
+        printf("%d - %s\n", i+1, nomes[i]);
+    scanf("%d", &opcao);
+    invoca(p[opcao-1]);
+
+    return 0;
+}
